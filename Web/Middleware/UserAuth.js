bb7ec8bd6bc630c3a63c6836dcf95b96a55eeabf
Zeeshan Ahmad Khalil <41861952+ZeeshanAhmadKhalil@users.noreply.github.com>
2021-11-07 13:13:05 -0800

    changes

--- Web/Middleware/UserAuth.js
+++ Web/Middleware/UserAuth.js
@@ -3,13 +3,16 @@ import config from 'config'
 
 export const UserAuth = (req, res, next) => {
     const token = req.header('x-auth-token')
+    console.log(token)
     if (!token)
         return res.status(410).json({ msg: "You are not authorized to access this feature" })
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'))
+        console.log(decoded)
         req.user = decoded.user
         next()
     } catch (error) {
-        return res.status(410).json(error)
+        console.log(error)
+        return res.status(410).json({ msg: "An unauthorized access is detected" })
     }
 }
