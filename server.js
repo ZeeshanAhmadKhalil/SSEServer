69eb039de56563ebc5904f4c81489e89e6bacf99
Zeeshan Ahmad Khalil <41861952+ZeeshanAhmadKhalil@users.noreply.github.com>
2021-11-27 20:11:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express()
 const router = express.Router()
 
 var corsOptions = {
-    origin: '*',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 //* some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
